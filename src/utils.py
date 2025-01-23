9a01c036461ac234eaff87b97cb9aae78ca0bb94
Chiebuka Ohams <chiebuka.ohams6@gmail.com>
2025-01-23 16:22:52 -0500

    utils

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,6 @@ import math
 import re
 import nltk
 from nltk.corpus import brown
-import spacy
 import pandas as pd
 from tqdm import tqdm
 import gensim.downloader
@@ -31,22 +30,29 @@ def encode_batch(batch, key2index, tokenizer):
         (ids+[key2index[PAD_TOKEN]]*(max_batch_len - len(ids)),
         [1]*len(ids) + [0]*(max_batch_len - len(ids))) for ids in batch_ids
     ]
-    #TODO: zero mask the [UNK] tokens
-    # separate into 2d lists of ids and masks
     padded_batch_ids, masks = list(zip(*padded_batch_ids_and_masks))
     batch["padded_batch_ids"] = padded_batch_ids
     batch["masks"] = masks
     return batch
 
-def clean_token(token):
-    token = re.sub("n't", "not", token)
-    token = re.sub("'m", "am", token)
-    token = re.sub("'re", "are", token)
-    token = re.sub("[^a-zA-Z0-9]", "", token).lower()
-    return token
+def clean_token(token, vocab):
+    token = token.lower()
+    if token in vocab:
+        final_token = token
+    else:
+        token_cand1 = re.sub("[^a-z0-9-]", "", token)
+        if token_cand1 in vocab:
+            final_token = token_cand1
+        else:
+            token_cand2 = token.split("-")[-1]
+            if token_cand2 in vocab:
+                final_token =  token_cand2
+            else:
+                final_token = token
+    return final_token
 
 
-def get_brown():
+def get_brown_old():
     id2sent_raw = dict()
     id2sent_clean = dict()
     id_count = 0
@@ -83,101 +89,104 @@ def get_brown():
         "sentence": id2sent_clean.values()
     })
     id2sent_clean_df.to_csv("./corpora/brown/clean_brown.csv", index=False)
-            
+
+
+def get_brown():
+    vocab = load_embeddings("glove-wiki-gigaword-300").index_to_key
+    id2sent_raw = dict()
+    id2sent_clean = dict()
+    id_count = 0
+    nltk.download('brown') # get data if not already available
+    for sentence in tqdm(brown.sents()):
+        cleaned_tokens = []
+        for token in sentence:
+            cleaned_token = clean_token(token, vocab)
+            if cleaned_token != "":
+                cleaned_tokens.append(cleaned_token)
+        raw_sentence = " ".join(sentence)
+        clean_sentence = " ".join(cleaned_tokens)
+        id2sent_raw[f"brown.{id_count}"] = raw_sentence
+        id2sent_clean[f"brown.{id_count}"] = clean_sentence
+        id_count += 1
+    id2sent_raw_df = pd.DataFrame.from_dict({
+        "id": id2sent_raw.keys(),
+        "sentence": id2sent_raw.values()
+    })
+    id2sent_raw_df.to_csv("./corpora/raw_brown_glove.csv", index=False)
+    id2sent_clean_df = pd.DataFrame.from_dict({
+        "id": id2sent_clean.keys(),
+        "sentence": id2sent_clean.values()
+    })
+    id2sent_clean_df.to_csv("./corpora/clean_brown_glove.csv", index=False)
+
+
 def split_brown(split):
     train, val, test = split
-
-    full_data = pd.read_csv("./corpora/brown/clean_brown.csv")
+    full_data = pd.read_csv("./corpora/clean_brown_glove.csv")
     shuffled_data = full_data.sample(frac=1)
-
     train_end_index = math.floor(len(shuffled_data)*train)
     val_end_index = math.floor(len(shuffled_data)*(train+val))
-
     train_data = shuffled_data[:train_end_index]
     val_data = shuffled_data[train_end_index:val_end_index]
     test_data = shuffled_data[val_end_index:]
+    train_data.to_csv("./corpora/train_brown.csv", index=False)
+    val_data.to_csv("./corpora/val_brown.csv", index=False)
+    test_data.to_csv("./corpora/test_brown.csv", index=False)
 
-    train_data.to_csv("./corpora/brown/train_brown.csv", index=False)
-    val_data.to_csv("./corpora/brown/val_brown.csv", index=False)
-    test_data.to_csv("./corpora/brown/test_brown.csv", index=False)
 
-
-def get_dundee():
-    df = pd.read_csv("metrics_full.csv")
+def get_dundee_sentences():
+    vocab = load_embeddings("glove-wiki-gigaword-300").index_to_key
     id2sent = dict()
     id_count = 0
     tokens = []
-    for index, row in df.iterrows():
-        if row["sentence_num"] != id_count:
+    with open("./text_files/dundee.txt", "r") as f:
+        for line in f:
+            for token in line.split():
+                tokens.append(clean_token(token, vocab))
             id2sent[id_count] = " ".join(tokens)
             id_count += 1
             tokens = []
-        tokens.append(clean_token(row["original_tokens"]))
-
-    id2sent[id_count] = " ".join(tokens)
-        
     id2sent_df = pd.DataFrame.from_dict({
         "id": id2sent.keys(),
         "sentence": id2sent.values()
     })
-    id2sent_df.to_csv("./corpora/dundee/dundee.csv", index=False)
+    id2sent_df.to_csv("./corpora/dundee.csv", index=False)
 
-
-def get_appleseed():
-    all_tokens = []
-    segment_nums = []
-    sent_nums = []
-    token_nums = []
-    sent_num = 0
-    token_num = 0
-    id2sent = dict()
-    segments = [f"{i+1}" for i in range(11)] + ['11b']
-    for seg in segments:
-        with open(f"./appleseed_text/appleseed{seg}.txt", "r") as f:
-            for line in f.readlines():
-                sent = []
-                tokens = line.split()
-                if len(tokens) > 0:
-                    for token in tokens:
-                        all_tokens.append(clean_token(token))
-                        sent.append(all_tokens[-1])
-                        segment_nums.append(seg)
-                        sent_nums.append(sent_num)
-                        token_nums.append(token_num)
-                        token_num += 1
-                    sent_num += 1
-                    id2sent[sent_num] = " ".join(sent)
-
-    id2sent_df = pd.DataFrame.from_dict({
-        "id": id2sent.keys(),
-        "sentence": id2sent.values()
-    })
-
-    tokens_df = pd.DataFrame({
-        "tokens": all_tokens,
-        "segment_num": segment_nums,
-        "sentence_num": sent_nums,
-        "token_num": token_nums
-    })
-
-    id2sent_df.to_csv("./corpora/appleseed.csv", index=False)
-    tokens_df.to_csv("./appleseed_base.csv", index=False)
-
-
-def get_natural_stories():
-    df = pd.read_csv("natural_stories_rts.csv")
+def get_natural_stories_sentences():
+    vocab = load_embeddings("glove-wiki-gigaword-300").index_to_key
     id2sent = dict()
     id_count = 0
     tokens = []
-    for index, row in df.iterrows():
-        tokens.append(clean_token(row["token"]))
-        if "." in row["token"]:
+    with open("./text_files/natural_stories.txt", "r") as f:
+        for line in f:
+            for token in line.split():
+                tokens.append(clean_token(token, vocab))
             id2sent[id_count] = " ".join(tokens)
             id_count += 1
             tokens = []
-
     id2sent_df = pd.DataFrame.from_dict({
         "id": id2sent.keys(),
         "sentence": id2sent.values()
     })
     id2sent_df.to_csv("./corpora/natural_stories.csv", index=False)
+
+def get_alice_sentences():
+    vocab = load_embeddings("glove-wiki-gigaword-300").index_to_key
+    df = pd.read_csv('./csvs/AliceChapterOne-EEG.csv')
+    id2sent = dict()
+    id_count = 0
+    tokens = []
+    prev_sent_num = 1
+    for index, row in df.iterrows():
+        if row["Sentence"] != prev_sent_num:
+            id2sent[id_count] = " ".join(tokens)
+            id_count += 1
+            tokens = []
+            prev_sent_num = row["Sentence"]
+        tokens.append(clean_token(row["Word"], vocab))
+    id2sent[id_count] = " ".join(tokens)
+    id2sent_df = pd.DataFrame.from_dict({
+        "id": id2sent.keys(),
+        "sentence": id2sent.values()
+    })
+    id2sent_df.to_csv("./corpora/alice.csv", index=False)
