e1455f85dc8f1c4dcb1d36074c09470a78d80b7d
Chiebuka Ohams <67585808+Chief-Buka@users.noreply.github.com>
2025-10-05 21:11:17 -0400

    Create eval_batch_ns.sh

---
+++ src/eval_batch_ns.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#SBATCH -t 06:00:00
+#SBATCH --mem=8GB
+#SBATCH --gres=gpu:1
+
+python3 extract_metrics.py --K 0 --model_path "../results/trained_models_linear_0_512_new/epoch_199.pt" --batch_size 64 --corpus_path "./corpora/natural_stories.csv" --savedir "../results/natural_stories_evals_linear_0_512_new" 
